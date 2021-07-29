8f56012913766a58ecb973c0f9add90fa72340a7
jmgomezs <jmgomezsocola@gmail.com>
2021-07-28 19:49:18 -0500

    Added DownloadData script for SC2

---
+++ ScintPi2/step0_SC2_downloaddata.py
@@ -0,0 +1,3 @@
+'''
+rsync -a --progress -e "ssh -i /home/jm/Documents/scintpi.key -p 22" scintpi@debye.utdallas.edu:/mfs/io/groups/uars/scintpi/sc000/scintpi2_20210314* /mnt/c/Users/JmGomezs/Documents/Scintpi/
+'''
